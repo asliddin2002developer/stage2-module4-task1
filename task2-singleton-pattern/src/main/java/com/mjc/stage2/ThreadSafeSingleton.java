d1fc0ac8fce4e387fba09e0b0288a57696759d34
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2023-02-11 18:57:54 +0500

    Implemented Creational Patterns such as Builder, Factory Method, Singleton to solve the tasks

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -3,22 +3,15 @@ package com.mjc.stage2;
 public class ThreadSafeSingleton {
     // Write your code here!
     private static volatile ThreadSafeSingleton instance;
-    private static Object mutex = new Object();
 
     private ThreadSafeSingleton(){
 
     }
 
-    public static ThreadSafeSingleton getInstance(){
-        ThreadSafeSingleton result = instance;
-        if (result == null){
-            synchronized (mutex){
-                result = instance;
-                if (result == null){
-                    instance = result = new ThreadSafeSingleton();
-                }
-            }
+    public static synchronized ThreadSafeSingleton getInstance(){
+        if (instance == null){
+            instance = new ThreadSafeSingleton();
         }
-        return result;
+        return instance;
     }
 }
