2fd877ef846796f15d17c282cc96d50914913607
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2023-02-11 18:49:47 +0500

    Implemented Creational Patterns such as Builder, Factory Method, Singleton to solve the tasks

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -2,4 +2,23 @@ package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
     // Write your code here!
+    private static volatile ThreadSafeSingleton instance;
+    private static Object mutex = new Object();
+
+    private ThreadSafeSingleton(){
+
+    }
+
+    public static ThreadSafeSingleton getInstance(){
+        ThreadSafeSingleton result = instance;
+        if (result == null){
+            synchronized (mutex){
+                result = instance;
+                if (result == null){
+                    instance = result = new ThreadSafeSingleton();
+                }
+            }
+        }
+        return result;
+    }
 }
--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,28 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
+import com.mjc.stage2.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
     // Write your code here!
+    @Override
+    public Connection createConnection(){
+        Connection connection = null;
+        Properties properties = new Properties();
+        try {
+            properties.load(H2ConnectionFactory.class.getClassLoader().getResourceAsStream("h2database.properties"));
+            connection = DriverManager.getConnection(
+                    properties.getProperty("db_url"),
+                    properties.getProperty("user"),
+                    properties.getProperty("password"));
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+        return connection;
+    }
+
 }
 
